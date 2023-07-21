6ea78b3fc95a579ebbb34c6ae51db0bfeaa22221
vivianpengnyc <yp2457@columbia.edu>
2023-07-21 17:07:52 -0400

    mina_contract

---
+++ mina_contract/Add.ts
@@ -0,0 +1,29 @@
+import { Field, SmartContract, state, State, method } from 'snarkyjs';
+
+/**
+ * Basic Example
+ * See https://docs.minaprotocol.com/zkapps for more info.
+ *
+ * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
+ * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
+ *
+ * This file is safe to delete and replace with your own contract.
+ */
+export class Add extends SmartContract {
+  @state(Field) address = State<Field>();
+  @state(Field) hasClaimed = State<Field>();
+ 
+
+  init() {
+    super.init();
+    this.address.set(Field("bhhhhhdd"));
+    this.hasClaimed.set(Field(false));
+  }
+
+  @method update() {
+    const currentState = this.address.getAndAssertEquals();
+    const newState = currentState.mul(false);
+    this.hasClaimed.set(newState);
+  }
+  
+}
