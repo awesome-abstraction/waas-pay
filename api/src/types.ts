344fffaad039f43fdbe0a4aae3291662abe0a13b
luxo-ai <luxo.ai@proton.me>
2023-07-22 22:52:13 +0200

    remove file

--- api/src/types.ts
+++
@@ -1,8 +0,0 @@
-import type { WalletFeature } from "./__generated__/types";
-
-export type DeserializedFeature = Omit<WalletFeature, "serializedParams"> & {
-  params: Record<string, any>;
-};
-
-export type AsyncReturnType<T extends (...args: any) => Promise<any>> =
-  T extends (...args: any) => Promise<infer R> ? R : any;
